fe87e27a368ec0ffae9ce8f6df4514b2ed7c4a3a
Alexandru Apostu <apostuionutalexandru@gmail.com>
2025-06-02 15:46:45 +0300

    feat: adding promises chaining, solving the old way of callbacks

--- minisprint-001/HomeWork1.js
+++ minisprint-001/HomeWork1.js
@@ -663,3 +663,37 @@ bookHotel('H002')
   .catch(error => {
     console.log('Error:', error);
   });
+
+
+console.log("\n--- 3. PROMISE CHAINING ---");
+
+function fetchUser(id) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ id, name: 'YB', profileId: 123 });
+    }, 200);
+  });
+}
+
+function fetchProfile(profileId) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ id: profileId, bio: 'BIO' });
+    }, 300);
+  });
+}
+
+
+// Promise chaining (solving callback hell)
+fetchUser(1)
+  .then(user => {
+    console.log('User fetched:', user);
+    return fetchProfile(user.profileId);
+  })
+  .then(profile => {
+    console.log('Profile fetched:', profile);
+    return fetchSettings(1);
+  })
+  .catch(error => {
+    console.log('Error in chain:', error.message);
+  });
